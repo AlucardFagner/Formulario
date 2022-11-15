68987b7740c4255304b0f2a8eb9730989c57f041
Alucard Casa <alucardfagner@yahoo.com>
2022-11-14 22:57:26 -0300

    Formulario Alucard Fagner

---
+++ Formulario-main/js/script.js
@@ -0,0 +1,19 @@
+$("#clique").click(function(){
+    $(this).css("color", "red");
+})
+
+$("#btn-submit").on("click",function (e){
+    let nome = $("#nome");
+    nome.css("border", "1px solid black");
+    $("p.erro-nome").hide()
+    if (nome.vall() =="") {
+        e.preventdefault();
+        nome.css("border", "1px solid red");
+        $("p").show()
+    }
+
+})
+
+$("#sumir").on("click", function (e) {
+    $("p").hide()
+})
